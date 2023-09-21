736d2eaaad7b44cfbd54219230561af5bc08c2ee
Efemena <88979259+Festivemena@users.noreply.github.com>
2023-09-21 22:13:47 +0000

    responsive header and hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -21,8 +21,8 @@ function CTA() {
   }, []);
 
   return (
-    <div className="pt-40 ml-24 w-full">
-      <div className="text-black text-[60px] w-full text-center font-bold">
+    <div className="pt-6 lg:pt-40 lg:ml-24 w-full">
+      <div className="text-black text-[38px] lg:text-[60px] w-full text-center font-bold">
         Do All With
         <span>
           <TextTransition
@@ -33,33 +33,33 @@ function CTA() {
           </TextTransition>
         </span>
       </div>
-      <div className="text-black text-center text-[20px] px-12 ml-10 pt-5 w-[481px]">
+      <div className="text-black px-6 text-center text-[20px] sm:w-full lg:px-12 lg:ml-10 pt-5 lg:w-[481px]">
         The card that simplifies payments, tracks attendance, and powers your
         ride with PayWayve platform.
       </div>
-      <div className="ml-16 mt-12">
+      <div className="pl-24 mt-6 items-center lg:ml-16 lg:mt-12">
         <div className="w-2/3 ">
           <div className="text-white bg-[#9A11FF] cursor-pointer w-full text-[20px] h-[62px] ml-6 shadow-2xl font-bold text-center py-4 rounded-md">
             Join PayWayve
           </div>
         </div>
         <div className="flex w-2/3 ml-6  gap-2 mt-5">
-          <div className="text-white pt-1 flex cursor-pointer bg-black w-1/2 h-[62px]  shadow-2xl rounded-md">
+          <div className="text-white pt-1 flex cursor-pointer bg-black w-1/2 h-[42px] lg:h-[62px]  shadow-2xl rounded-md">
             <div className="flex">
-              <DiApple className="w-[48px] h-[48px]" />
+              <DiApple className="w-[36px] h-[36px] lg:w-[48px] lg:h-[48px]" />
             </div>
-            <div className="flex-1">
-              <div className="text-[12px]">Download on the</div>
-              <div className="text-[14px] font-bold">Apple Store</div>
+            <div className="flex-1 py-2">
+              <div className="lg:text-[12px] text-[8px]">Download on</div>
+              <div className=" lg:text-[14px] text-[10px] font-bold">Apple Store</div>
             </div>
           </div>
-          <div className="text-white pt-1 cursor-pointer flex bg-black w-1/2 h-[62px]  shadow-2xl rounded-md">
+          <div className="text-white pt-1 cursor-pointer flex bg-black w-1/2 h-[42px] lg:h-[62px] shadow-2xl rounded-md">
             <div className="flex">
-              <BiLogoPlayStore className="w-[48px] h-[48px]" />
+              <BiLogoPlayStore className="w-[36px] h-[36px] lg:w-[48px] lg:h-[48px]" />
             </div>
-            <div className="flex-1">
-              <div className="text-[12px] pt-2">Get it on</div>
-              <div className="text-[14px] font-bold">
+            <div className="flex-1 py-2">
+              <div className="lg:text-[12px] text-[8px]">Get it on</div>
+              <div className="lg:text-[14px] text-[10px] font-bold">
                 <a className="text-[#0086F9]">G</a>
                 <a className="text-[#FF4131]">o</a>
                 <a className="text-[#FEBD00]">o</a>
@@ -78,11 +78,11 @@ function CTA() {
 
 export default function Hero() {
   return (
-    <div className="w-full h-full flex">
-      <div className="px-24">
+    <div className="w-full h-full flex-1 lg:flex">
+      <div className="lg:px-24">
         <CTA />
       </div>
-      <div className="">
+      <div className="sm:w-full sm:pl-6">
         <Image alt="Hero" src={HeroPic} />
       </div>
     </div>
