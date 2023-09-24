128f0cd43c719e0c3f19e8efa80f4b56d469e162
Efemena <88979259+Festivemena@users.noreply.github.com>
2023-09-24 21:18:02 +0000

    linkk

--- src/components/invite.js
+++ src/components/invite.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from 'next/image'
+import Link from 'next/link'
 import { HeroPic, OneTap, Ui, Logo, Subtract } from "../../public";
 
 export default function Invite() {
