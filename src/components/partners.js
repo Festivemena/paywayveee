a2af3973e85e748b279d86c243c6c9dcca268bf6
Efemena <88979259+Festivemena@users.noreply.github.com>
2023-09-25 11:19:23 +0000

    partners

--- src/components/partners.js
+++ src/components/partners.js
@@ -6,10 +6,10 @@ export default function Partners() {
   return (
     <div className="flex-1 bg-white pb-36 items-center justify-between w-full">
       <div className="text-[38px] pb-10 justify-center text-black flex text-center font-bold">Our Partners</div>
-      <div className="flex justify-center space-x-20">
-        <Image className="h-36 w-40" alt="WemaBank" src={WemaBank} />
-        <Image className="h-36 w-40" alt="UnibenMFB" src={UnibenMfb} />
-        <Image className="h-36 w-40" alt="Paystack" src={PayStack} />
+      <div className="flex justify-center space-x-4 lg:space-x-20">
+        <Image className="lg:h-36  w-32 h-28 lg:w-40" alt="WemaBank" src={WemaBank} />
+        <Image className="lg:h-36  w-32 h-28 lg:w-40" alt="UnibenMFB" src={UnibenMfb} />
+        <Image className="lg:h-36  w-32 h-28 lg:w-40" alt="Paystack" src={PayStack} />
       </div>
     </div>
   );
