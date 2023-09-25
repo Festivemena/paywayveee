18f6c42604470d5c6c208c4f729a7ac103bf48bc
Efemena <88979259+Festivemena@users.noreply.github.com>
2023-09-25 11:16:25 +0000

    testimonials

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -54,11 +54,11 @@ function Individual() {
 function IndividualMobile() {
   return (
     <div className=" shadow-3xl lg:-mt-[600px] absolute -mt-[500px] text-black justify-between items-center flex-1 lg:flex z-20 ">
-      <div className="lg:px-20 mr-10 flex-1 lg:flex">
-      <div className="lg:w-1/2 w-full py-2 px-4 mr-5 bg-white shadow-2xl rounded-lg ">
+      <div className="lg:px-20 px-4 flex-1 lg:flex">
+      <div className="lg:w-1/2 w-full py-2 px-4 bg-white shadow-2xl rounded-lg ">
         <div className="w-full flex">
           <Image className="w-24 h-24 lg:w-32 lg:h-32 rounded-full" alt="Ijeoma"  src={Ijeoma} />
-          <div className="flex-1 pl-6 py-8 ">
+          <div className="flex-1 pl-6 py-4 ">
             <div className="lg:text-[20px] text-[16px] font-bold">Ijeoma Ogiokpe</div>
             <div className="lg:text-[18px] text-[14px]">News caster</div>
           </div>
@@ -66,34 +66,34 @@ function IndividualMobile() {
         <div className="text-[12px] lg:text-[16px] bg-[#F2FCFF] py-2 text-left px-4 lg:px-16 mt-1 lg:mt-8">
         Using the Paywayve Card has truly transformed the way I handle my daily transactions.
         </div>
-        <div className="px-6 pt-2 lg:pt-8">22 Feb 2023 . 8:15 AM</div>
+        <div className="px-6 text-[10px] pt-2 lg:pt-8">22 Feb 2023 . 8:15 AM</div>
       </div>
       <div className="lg:w-1/2 w-full shadow-2xl justify-between rounded-lg flex-1">
-        <div className="w-full bg-white p-4 h-1\2 rounded-lg">
+        <div className="w-full bg-white mt-1 p-4 h-1\2 rounded-lg">
         <div className="w-full flex">
           <Image className="w-24 h-24 lg:w-32 lg:h-32 rounded-full" alt="Kingsley" src={Kingsley} />
-          <div className="flex-1 pl-6 py-8 ">
-            <div className="text-[20px] font-bold">Kingsley Peters</div>
-            <div className="text-[18px]">Chemical Engineer</div>
+          <div className="flex-1 pl-6 py-4 ">
+            <div className="lg:text-[20px] text-[16px] font-bold">Kingsley Peters</div>
+            <div className="lg:text-[18px] text-[14px]">Chemical Engineer</div>
           </div>
         </div>
-        <div className="text-[12px] lg:text-[16px] bg-[#F2FCFF] py-2 text-left px-4 lg:px-16 mt-2 lg:mt-8">
+        <div className="text-[12px] lg:text-[16px] bg-[#F2FCFF] py-2 text-left px-4 lg:px-16 mt-1 lg:mt-8">
         Paywayve Card simplifies my life – tap for payments, attendance, and ride payments. Secure, accepted everywhere, and eco-friendly. It&apos;s a game-changer!
         </div>
-        <div className="px-6 pt-2 lg:pt-8">22 Feb 2023 . 8:15 AM</div>
+        <div className="px-6 pt-2 text-[10px] lg:pt-8">22 Feb 2023 . 8:15 AM</div>
         </div>
-        <div className="w-full bg-white mt-9 p-4 h-1\2 rounded-lg">
+        <div className="w-full bg-white mt-1 p-4 h-1\2 rounded-lg">
         <div className="w-full flex">
           <Image className="w-24 h-24 lg:w-32 lg:h-32 rounded-full" alt="Justin" src={Justin} />
-          <div className="flex-1 pl-6 py-8 ">
-            <div className="text-[20px] font-bold">Justin Oluremi</div>
-            <div className="text-[18px]">Student</div>
+          <div className="flex-1 pl-6 py-4 ">
+            <div className="lg:text-[20px] text-[16px] font-bold">Justin Oluremi</div>
+            <div className="lg:text-[18px] text-[14px]">Student</div>
           </div>
         </div>
-        <div className="text-[14px] bg-[#F2FCFF] py-2 text-left px-6 mt-8">
+        <div className="text-[12px] lg:text-[16px] bg-[#F2FCFF] py-2 text-left px-4 lg:px-16 mt-1 lg:mt-8">
         Paywayve Card has revolutionized my transac...
         </div>
-        <div className="px-6 pt-2 lg:pt-8">22 Feb 2023 . 8:15 AM</div>
+        <div className="px-6 pt-2 text-[10px] lg:pt-8">22 Feb 2023 . 8:15 AM</div>
         </div>
       </div>
       </div>
