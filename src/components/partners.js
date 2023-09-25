f2566b7b31c431415a54c8138c0036eb1d87476b
Efemena <88979259+Festivemena@users.noreply.github.com>
2023-09-25 11:31:01 +0000

    partners

--- src/components/partners.js
+++ src/components/partners.js
@@ -4,12 +4,12 @@ import { PayStack, UnibenMfb, WemaBank } from "../../public";
 
 export default function Partners() {
   return (
-    <div className="flex-1 bg-white pb-36 items-center justify-between w-full">
-      <div className="text-[38px] pb-10 justify-center text-black flex text-center font-bold">Our Partners</div>
+    <div className="flex-1 bg-white pb-6 lg:pb-36 items-center justify-between w-full">
+      <div className="text-[38px] pb-4 lg:pb-10 justify-center text-black flex text-center font-bold">Our Partners</div>
       <div className="flex justify-center space-x-4 lg:space-x-20">
-        <Image className="lg:h-36  w-32 h-28 lg:w-40" alt="WemaBank" src={WemaBank} />
-        <Image className="lg:h-36  w-32 h-28 lg:w-40" alt="UnibenMFB" src={UnibenMfb} />
-        <Image className="lg:h-36  w-32 h-28 lg:w-40" alt="Paystack" src={PayStack} />
+        <Image className="lg:h-36  w-28 h-24 lg:w-40" alt="WemaBank" src={WemaBank} />
+        <Image className="lg:h-36  w-28 h-24 lg:w-40" alt="UnibenMFB" src={UnibenMfb} />
+        <Image className="lg:h-36  w-28 h-24 lg:w-40" alt="Paystack" src={PayStack} />
       </div>
     </div>
   );
